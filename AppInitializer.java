b0bb69c2fdf915c6933a8071abaa2da2f802295c
avishkauwanpriya <56757576+avishkauwanpriya@users.noreply.github.com>
2020-07-17 12:32:48 +0530

    created and modified AppInitializer (#2)

---
+++ AppInitializer.java
@@ -0,0 +1,7 @@
+public class AppInitializer{
+    public static void main(String [] args){
+        System.out.println("Avishka Uwanpriya");
+        System.out.println("Nadeepa Chathuranga");
+    }
+
+}
