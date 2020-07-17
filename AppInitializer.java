bc3d0bf42f0d271d209f28cfff820d30a91d7269
avishkauwanpriya <56757576+avishkauwanpriya@users.noreply.github.com>
2020-07-17 14:13:06 +0530

    Modified AppInitializer.java Check it out !!!!!!!! (#3)
    * created and modified AppInitializer
    
    * modified AppInitializer class
    
    Co-authored-by: Nadeepa Chathuranga <52320494+nchathura@users.noreply.github.com>

--- AppInitializer.java
+++ AppInitializer.java
@@ -2,6 +2,7 @@ public class AppInitializer{
     public static void main(String [] args){
         System.out.println("Avishka Uwanpriya");
         System.out.println("Nadeepa Chathuranga");
+        System.out.println("Gon Avishka");
     }
 
 }
